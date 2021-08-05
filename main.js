d08da56f240bd6d70069423be34926944dc266ed
Lumensum <roy.schrauwen@gmail.com>
2021-08-05 19:58:32 +0200

    Refactoring

--- main.js
+++ main.js
@@ -4,44 +4,37 @@ const inputField = document.getElementById("input_email");
 
 const errorEmpty = "Whoops! It looks like you forgot to add your email";
 const errorInvalid = "Please provide a valid email address";
+const emailValidationMessage = document.getElementById("validationmessage");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("form submitted");
-  console.log(ValidateEmail());
+  ValidateEmail();
 });
 
 function ValidateEmail() {
-  console.log("Trying to Validate Email");
-
   const validEmailFormat =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   // ToDO: Research RFC 2822
 
   var inputtedEmail = form["input_email"].value;
 
-  if (inputtedEmail == "") {
-    validationmessageContainer.innerText = errorEmpty;
-    inputField.classList.add("invalid");
-    validationmessageContainer.classList.remove("hideMe");
-    console.log(errorEmpty);
-    return;
-  }
-
-  const emailValidationMessage = document.getElementById("validationmessage");
-
   if (inputtedEmail.match(validEmailFormat)) {
     inputField.classList.remove("invalid");
     validationmessageContainer.classList.add("hideMe");
     validationmessageContainer.innerText = "";
-
     return true;
-  } else {
-    console.log(errorInvalid);
-    inputField.classList.add("invalid");
-    validationmessageContainer.innerText = errorInvalid;
-    validationmessageContainer.classList.remove("hideMe");
+  }
 
+  if (inputtedEmail == "") {
+    validationmessageContainer.innerText = errorEmpty;
+    inputField.classList.add("invalid");
+    validationmessageContainer.classList.remove("hideMe");
     return false;
   }
+
+  inputField.classList.add("invalid");
+  validationmessageContainer.innerText = errorInvalid;
+  validationmessageContainer.classList.remove("hideMe");
+
+  return false;
 }
